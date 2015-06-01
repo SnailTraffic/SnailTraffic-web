cf3f7f830c8f14d4011a317158cc9f0f0c388c57
LinGoWei <weiliu32992016@qq.com>
2015-06-01 14:31:59 +0800

    团队1.9.2版
    地图显示公交线路

--- src/com/snail/traffic/container/transfer/TransitScheme.java
+++ src/com/snail/traffic/container/transfer/TransitScheme.java
@@ -13,31 +13,28 @@ import com.snail.traffic.container.info.CovertToJsonObject;
  */
 public class TransitScheme extends BaseTransitStruct implements CovertToJsonObject {
 	public Vector<TransitSection> transitSections = new Vector<>();
-	public String time = null;
-	public String distance = null;
+	public int time = 0;
+	public int distance = 0;
 
 	@Override
 	public JSONObject toJSONObject() {
 		JSONObject o = new JSONObject();
 		JSONArray a = new JSONArray();
 
-		time = "";
-		distance = "";
-
 		String title = "";
-
+		String t = this.time + "";
+		String d = this.distance + "";
 		try {
 			for (int i = 0; i < transitSections.size(); i++) {
 				a.add(transitSections.get(i).toJSONObject());
-
 				if (i > 0) { title += " - "; }
-				title += transitSections.get(i).lineNumber;
+				title += transitSections.get(i).lineName;
 			}
 
 			o.put("title", title);
 			o.put("scheme", a);
-			o.put("time", time);
-			o.put("distance", distance);
+			o.put("time", t);
+			o.put("distance", d);
 		} catch (Exception e){
 			e.printStackTrace();
 			o = null;
--- src/com/snail/traffic/control/query/SelectTransfer.java
+++ src/com/snail/traffic/control/query/SelectTransfer.java
@@ -14,33 +14,36 @@ public class SelectTransfer {
 	Connection con = null;
 	SelectAccessProcedure selAccess = null;
 
-	// ���캯��
+	/**
+	 * constructor
+	 * @param con
+	 */
 	public SelectTransfer(Connection con) {
 		this.con = con;
 	}
 
 	/**
-	 * ���˲�ѯ
-	 * ���ȼ���Ƿ�ֱ��
-	 * ����Ƿ����һ�λ���
+	 * 换乘查询
+	 * 首先检查是否直达
+	 * 检查是否存在一次换乘
 	 * @param start
-	 * 			��ʼ�ϳ�վ��
+	 * 	起始上车站点
 	 * @param end
-	 * 			�����³�վ��
+	 * 	最终下车站点
 	 * @return
 	 */
 	public InfoStruct query(String start, String end) {
 		if (start == null || end == null)
 			return null;
 
-		// ����ʼվ������ֹվ���ظ�ʱ
+		// 当起始站点与终止站点重复时
 		if (start.equals(end)) 
 			return null;
 		
-		TotalTimePriorityQueue allSchemes = new TotalTimePriorityQueue();// ���շ������ȶ���
+		TotalTimePriorityQueue allSchemes = new TotalTimePriorityQueue();// 锟斤拷锟秸凤拷锟斤拷锟斤拷锟饺讹拷锟斤拷
 		selAccess = new SelectDirectAccessProcedure(con);
 
-		BaseAccessVector directV = new DirectAccessVector();	// ֱ��վ�㼯��
+		BaseAccessVector directV = new DirectAccessVector();	// 直锟斤拷站锟姐集锟斤拷
 		directV.relateSite = start;
 		directV.relateVector = selAccess.getAccessSites(start);
 
@@ -48,11 +51,10 @@ public class SelectTransfer {
 		ret.start = start;
 		ret.end = end;
 
-		// ������������3��
+		// 若方案数大于3个
 		if (through(allSchemes, directV, end) >= 3) {
-			System.out.println("��ֱ����·������");
+			System.out.println("3 lines");
 		}
-		// ����ȡֱ�﷽���󣬷�����С��3��
 		else {
 			BaseAccessVector be_directV = new BeDirectAccessVector();
 			selAccess = new SelectBeDirectAccessProcedure(con);
@@ -61,41 +63,40 @@ public class SelectTransfer {
 			Set<String> setIntersection = intersection(directV.getSiteSetFromVector(), be_directV.getSiteSetFromVector());
 			
 			if (onceTransfer(allSchemes, directV, be_directV, setIntersection) >= 3)
-				System.out.println("ֱ�����һ�λ��˵���·������");
+				System.out.println("has 3 lines");
 			else
-				System.out.println("��չ���λ�������");
+				System.out.println("has not 3 lines");
 		}
 		ret.schemes = getPriorityRoute(allSchemes);
 		return ret;
 	}
 	
 	/**
-	 * ����Ŀ��վ���ȡֱ�﷽��
-	 * һ��ֱ��˳���������һ������
+	 * 根据目标站点获取直达方案
+	 * 一个直达乘车方案就是一个方案
 	 * @param Scheme
-	 * 			��������
+	 * 			方案向量
 	 * @param vector
-	 * 			��ʼվ������
+	 * 			起始站点向量
 	 * @param end
-	 * 			Ŀ��վ��
+	 * 			目标站点
 	 * @return size
-	 * 			������
+	 * 			方案数
 	 */
 	private int through(TotalTimePriorityQueue allScheme, BaseAccessVector direct, String end) {
-		TimePriorityQueue transitVector = direct.getVectorTo(end); // �����ֱ���յ��������·
+		TimePriorityQueue transitVector = direct.getVectorTo(end); // 锟斤拷锟斤拷锟街憋拷锟斤拷盏锟斤拷锟斤拷锟斤拷锟斤拷路
 		TransitScheme newScheme = null;
 		TransitSection section = null;
 		int size = transitVector.size();
 
 		for (int i = 0; i < size; i++) {
 			newScheme = new TransitScheme();
-			section = transitVector.poll();	// ������ȶ����е�ʱ������Ԫ��
+			section = transitVector.poll();	// 锟斤拷锟斤拷锟斤拷榷锟斤拷锟斤拷械锟绞憋拷锟斤拷锟斤拷锟皆拷锟�
 			newScheme.transitSections.add(section);
 			newScheme.time = section.time;
 			newScheme.distance = section.distance;
 			allScheme.add(newScheme);
 		}
-		// System.out.println("ֱ�" + allScheme.size());
 		return allScheme.size();
 	}
 
@@ -121,18 +122,18 @@ public class SelectTransfer {
 				section.route = selRoute.getRoute(section.startSite, section.endSite, section.lineName, section.isLeft);
 			}
 			v.add(scheme);
-			if (i == 4)    // ��5������������
-				break;
+			if (i == 4)
+			    break;
 		}
 		return v;
 	}
 
 	/**
-	 * �����������󽻼�(׼ȷ)
+	 * 对两个集合求交集(准确)
 	 * @param setA
-	 * 			A����
+	 * 			A集合
 	 * @param setB
-	 * 			B����
+	 * 			B集合
 	 * @return
 	 */
 	private Set<String> intersection(Set<String> setA, Set<String> setB) {
@@ -148,28 +149,28 @@ public class SelectTransfer {
 	}
 
 	/**
-	 * һ�λ��˲�ѯ
+	 * 一次换乘查询
 	 * @param allScheme
-	 * 			���з���
+	 * 			所有方案
 	 * @param direct
-	 * 			��ʼվ��ֱ��վ��
+	 * 			起始站点直达站点
 	 * @param be_direct
-	 * 			��ֹվ�㱻ֱ��վ��
+	 * 			终止站点被直达站点
 	 * @param interset
-	 * 			�������ɻ���վ�㣩
-	 * @return ��ǰ������
+	 * 			交集（可换乘站点）
+	 * @return 当前方案数
 	 */
 	private int onceTransfer(TotalTimePriorityQueue allScheme
-							, BaseAccessVector direct
-							, BaseAccessVector be_direct
-							, Set<String> interSet) {
-		TransitScheme newScheme 				= null;	// �µķ���
-		TimePriorityQueue firstSectionQueue 	= null;	// ��һ�ι��������п�ѡ�������ȶ���
-		TimePriorityQueue secondSectionQueue 	= null;	// �ڶ��ι��������п�ѡ�������ȶ���
-		TransitSection firstSection 			= null;	// ��һ�ι���
-		TransitSection secondSection 			= null;	// �ڶ��ι���
+				, BaseAccessVector direct
+				, BaseAccessVector be_direct
+				, Set<String> interSet) {
+		TransitScheme newScheme 		= null;	// new scheme
+		TimePriorityQueue firstSectionQueue 	= null;	
+		TimePriorityQueue secondSectionQueue 	= null;	
+		TransitSection firstSection 		= null;
+		TransitSection secondSection 		= null;
 
-		// ѭ���ɻ���վ��
+		// 循环可换乘站点
 		for (String s: interSet) {
 			System.out.println(s);
 
@@ -181,7 +182,6 @@ public class SelectTransfer {
 			System.out.println(firstSection.lineName);
 			System.out.println(secondSection.lineName);
 			
-			// �����·���Ԫ��
 			newScheme = new TransitScheme();
 			newScheme.transitSections.add(firstSection);
 			newScheme.transitSections.add(secondSection);
@@ -189,17 +189,26 @@ public class SelectTransfer {
 			newScheme.distance = firstSection.distance + secondSection.distance;
 			allScheme.add(newScheme);
 		}
-		System.out.println("ֱ��+һ�λ���" + allScheme.size());
+		System.out.println("直达+一次换乘" + allScheme.size());
 		return allScheme.size();
 	}
 
-	/*
+	/**
+	 * 二次换乘
+	 * @return
+	 */
+	private int twiceTransfer(TotalTimePriorityQueue allScheme
+				, ) {
+	    
+	    
+	}
 	
+	/*
 	public static void main(String[] args) {
 		OracleBase oracle = new OracleBase();
 		Connection con = oracle.getConnection();
 		SelectTransfer a = new SelectTransfer(con);
-		Vector<TransitScheme> v = a.query("������˫��", "����·᷼�ɽ");
+		Vector<TransitScheme> v = a.query("锟斤拷锟斤拷锟斤拷双锟斤拷", "锟斤拷锟斤拷路岱硷拷山");
 		TransitScheme tt = null;
 		TransitSection rr = null;
 		
